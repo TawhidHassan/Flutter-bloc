3ef03d85cda44babb7553e2e2ef7b45d862a2f9c
sifat hassan <sifat007.sh7@gmail.com>
2021-04-29 14:32:31 +0600

    bloc comunication on under constratction

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_complete/cubit/counter_cubit.dart';
+import 'package:flutter_bloc_complete/presentation/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,125 +11,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
+    return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          // TODO: implement listener}
-          if (state.wasIncremented == true) {
-            Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("data Incremneted"),
-                  duration: Duration(milliseconds: 100),
-                )
-            );
-          } else {
-            Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("data Decremented"),
-                  duration: Duration(milliseconds: 300),
-                )
-            );
-          }
-        },
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  BlocConsumer<CounterCubit, CounterState>(
-                    listener: (context, state) {
-                      // TODO: implement listener
-                      if (state.wasIncremented == true) {
-                        Scaffold.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(" Incremneted show for blockConsumer"),
-                              duration: Duration(milliseconds: 200),
-                            )
-                        );
-                      }
-                    },
-                    builder: (context, state) {
-                      return Text(
-                        state.counterValue.toString()+" for blocConsumer",
-                      );
-                    },
-                  ),
-                  BlocBuilder<CounterCubit, CounterState>(
-                    builder: (context, state) {
-                      return Text(
-                        state.counterValue.toString(),
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .headline4,
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    // BlocProvider.of<CounterCubit>(context).increment();
-                    context.bloc<CounterCubit>().increment();
-                  },
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).dicrement();
-                  },
-                  tooltip: 'Dicrement',
-                  child: Icon(Icons.subdirectory_arrow_left),
-                ),
-              ],
-            )
-          ],
+        home: BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(),
+          child: HomeScreen(title: 'Flutter Demo Home Page',color: Colors.red,),
         ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
   }
 }
+
+
---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_complete/business_logic/cubit/counter_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_complete/presentation/screens/secound_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key, this.title,this.color}) : super(key: key);
+
+
+  final String title;
+  final Color color;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          // TODO: implement listener}
+          if (state.wasIncremented == true) {
+            Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("data Incremneted"),
+                  duration: Duration(milliseconds: 100),
+                )
+            );
+          } else {
+            Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text("data Decremented"),
+                  duration: Duration(milliseconds: 300),
+                )
+            );
+          }
+        },
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  BlocConsumer<CounterCubit, CounterState>(
+                    listener: (context, state) {
+                      // TODO: implement listener
+                      if (state.wasIncremented == true) {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(" Incremneted show for blockConsumer"),
+                              duration: Duration(milliseconds: 200),
+                            )
+                        );
+                      }
+                    },
+                    builder: (context, state) {
+                      return Text(
+                        state.counterValue.toString()+" for blocConsumer",
+                      );
+                    },
+                  ),
+                  BlocBuilder<CounterCubit, CounterState>(
+                    builder: (context, state) {
+                      return Text(
+                        state.counterValue.toString(),
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headline4,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // BlocProvider.of<CounterCubit>(context).increment();
+                    context.bloc<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).dicrement();
+                  },
+                  tooltip: 'Dicrement',
+                  child: Icon(Icons.subdirectory_arrow_left),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            MaterialButton(
+              color: Colors.greenAccent,
+              child: Text(
+                'Go to Third Screen',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SecondScreen(title: "Scond screen", color: Colors.blue,)));
+              },
+            ),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
