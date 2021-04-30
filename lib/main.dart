194fe020faab007d345df1461b5de8bd9dab116d
sifat hassan <sifat007.sh7@gmail.com>
2021-04-30 14:31:34 +0600

    HydratedBloc implementation

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_complete/business_logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_complete/business_logic/cubit/settings_cubit.dart';
 import 'package:flutter_bloc_complete/presentation/router/app_router.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'business_logic/cubit/internet_cubit.dart';
 
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HydratedBloc.storage=await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
   runApp(MyApp(
     appRouter: AppRouter(),
     connectivity: Connectivity(),
