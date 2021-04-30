bc466cf329c4bf5323c8cd54fac4d92f1106de0e
sifat hassan <sifat007.sh7@gmail.com>
2021-04-30 15:15:53 +0600

    store counter data

--- lib/business_logic/cubit/counter_state.dart
+++ lib/business_logic/cubit/counter_state.dart
@@ -8,4 +8,26 @@ class CounterState {
     @required this.counterValue,
     this.wasIncremented,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'counterValue': counterValue,
+      'wasIncremented': wasIncremented,
+    };
+  }
+
+  factory CounterState.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return CounterState(
+      counterValue: map['counterValue'],
+      wasIncremented: map['wasIncremented'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CounterState.fromJson(String source) =>
+      CounterState.fromMap(json.decode(source));
+
 }
